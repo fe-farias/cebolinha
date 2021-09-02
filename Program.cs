3ba1ca654b595957e312ae778efdb69b9dd632dc
Felipe Farias <fefarias97@gmail.com>
2021-09-02 15:55:59 -0300

    Resolução exercício cebolinha

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frasedigitada, frasecomdislalia;
+            Console.WriteLine("Digite uma flase: ");
+            frasedigitada = Console.ReadLine();
+
+            frasecomdislalia = frasedigitada.Replace("r", "l").Replace("R","L");
+
+            Console.WriteLine(frasecomdislalia);
+
+
+        }
+    }
+}
